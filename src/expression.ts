0d1afe2fc5012ff87c0b20dc75e4133b44496c03
belopash <abelopashentsev@gmail.com>
2025-05-21 23:28:55 +0300

    save

--- src/expression.ts
+++ src/expression.ts
@@ -46,33 +46,25 @@ export enum OpType {
   Parentheses,
 }
 
-interface OperatorConfig {
-  type: OpType;
-  associativity: 'left' | 'right';
-}
-
 interface Token {
   type: OpType;
   eval: (ctx: any, path: string[]) => { value: unknown; path: string[] };
-  //variables: (prefix: string[]) => string[];
 }
 
 export const EXPR_PATTERN = /(\${{[^}]*}})/;
 
 export class Parser {
-  constructor() {}
-
   parse(str: string): Expression {
     const tokens: (string | Token)[] = [];
-
     let pos = 0;
-    str.split(EXPR_PATTERN).map(i => {
+
+    str.split(EXPR_PATTERN).forEach(i => {
       if (EXPR_PATTERN.test(i)) {
         const token = new Tokenizer(i.slice(3, i.length - 2), pos + 3).parse();
         if (token) {
           tokens.push(token);
         }
-      } else if (!!i) {
+      } else if (i) {
         tokens.push(i);
       }
       pos += i.length;
@@ -81,12 +73,6 @@ export class Parser {
     return new Expression(tokens);
   }
 }
-
-const nums = new Set([...'0123456789']);
-const alpha = new Set([...'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ']);
-const alphaNum = new Set([...nums, ...alpha]);
-const identifiers = new Set([...alphaNum, '_', '-']);
-const whitespace = new Set([...' \t\n\r']);
 
 export class Tokenizer {
   private pos = 0;
@@ -110,9 +96,7 @@ export class Tokenizer {
 
     while (this.whitespace()) {
       const token = this.binary(res, precedence);
-      if (!token) {
-        break;
-      }
+      if (!token) break;
       res = token;
     }
 
@@ -124,19 +108,15 @@ export class Tokenizer {
   }
 
   private or(left: Token | undefined, precedence: number): Token | undefined {
-    if (this.str[this.pos] !== '|' || this.str[this.pos + 1] !== '|') return;
-    if (precedence > OpType.Or) return;
+    if (!this.match('||') || precedence > OpType.Or) return;
+    if (!left) throw this.unexpectedToken('||', this.pos);
 
     const start = this.pos;
-
-    if (!left) {
-      throw this.unexpectedToken('||', start);
-    }
     this.pos += 2;
+
     const right = this.next(OpType.Or + 1);
-    if (!right) {
-      throw this.unexpectedToken('||', start);
-    }
+    if (!right) throw this.unexpectedToken('||', start);
+
     return {
       type: OpType.Or,
       eval: (ctx, path) => {
@@ -147,19 +127,14 @@ export class Tokenizer {
   }
 
   private access(left: Token | undefined, precedence: number): Token | undefined {
-    if (this.str[this.pos] !== '.') return;
-    if (precedence > OpType.MemberAccess) return;
-
-    if (!left) {
-      throw this.unexpectedToken('.', this.pos);
-    }
+    if (!this.match('.') || precedence > OpType.MemberAccess) return;
+    if (!left) throw this.unexpectedToken('.', this.pos);
 
     const start = this.pos;
     this.pos++;
+
     const right = this.next(OpType.MemberAccess + 1);
-    if (!right) {
-      throw this.unexpectedToken('.', start);
-    }
+    if (!right) throw this.unexpectedToken('.', start);
 
     return {
       type: OpType.MemberAccess,
@@ -175,16 +150,19 @@ export class Tokenizer {
   }
 
   private paren(): Token | undefined {
-    if (this.str[this.pos] !== '(') return;
+    if (!this.match('(')) return;
 
     const start = this.pos;
     this.pos++;
+
     const expr = this.next(0);
     this.whitespace();
-    if (expr === undefined) return;
-    if (this.str[this.pos] !== ')') {
+
+    if (!expr) return;
+    if (!this.match(')')) {
       throw this.unexpectedToken(this.str.slice(start, this.pos), start);
     }
+
     this.pos++;
     return {
       type: OpType.Parentheses,
@@ -193,15 +171,14 @@ export class Tokenizer {
   }
 
   private string(): Token | undefined {
-    if (this.str[this.pos] !== "'") return;
-
+    if (!this.match("'")) return;
     this.pos++;
-    let result = '';
 
-    while (this.str[this.pos]) {
-      if (this.str[this.pos] === "'") {
+    let result = '';
+    while (this.pos < this.str.length) {
+      if (this.match("'")) {
         this.pos++;
-        if (this.str[this.pos] !== "'") {
+        if (!this.match("'")) {
           this.pos++;
           return {
             type: OpType.StringLiteral,
@@ -209,8 +186,7 @@ export class Tokenizer {
           };
         }
       }
-      result += this.str[this.pos];
-      this.pos++;
+      result += this.str[this.pos++];
     }
 
     throw this.unexpectedToken("'" + result, this.pos - result.length - 1);
@@ -218,8 +194,8 @@ export class Tokenizer {
 
   private id(): Token | undefined {
     const start = this.pos;
-    while (identifiers.has(this.str[this.pos])) {
-      if (this.str[this.pos] === '-' && !alphaNum.has(this.str[this.pos + 1])) {
+    while (this.isIdentifierChar(this.str[this.pos])) {
+      if (this.str[this.pos] === '-' && !this.isAlphaNum(this.str[this.pos + 1])) {
         throw this.unexpectedToken('-', this.pos);
       }
       this.pos++;
@@ -235,23 +211,45 @@ export class Tokenizer {
         }
 
         const newPath = [...path, id];
-
         if (ctx.hasOwnProperty(id)) {
           return { value: ctx[id], path: newPath };
         } else if (path.length === 0) {
           throw new UndefinedVariableError(newPath);
-        } else {
-          return { value: undefined, path: newPath };
         }
+        return { value: undefined, path: newPath };
       },
     };
   }
 
   private whitespace(): boolean {
-    while (whitespace.has(this.str[this.pos])) {
+    while (this.isWhitespace(this.str[this.pos])) {
       this.pos++;
     }
-    return !!this.str[this.pos];
+    return this.pos < this.str.length;
+  }
+
+  private match(expected: string): boolean {
+    return this.str.slice(this.pos, this.pos + expected.length) === expected;
+  }
+
+  private isWhitespace(char: string): boolean {
+    return char === ' ' || char === '\t' || char === '\n' || char === '\r';
+  }
+
+  private isAlphaNum(char: string): boolean {
+    return this.isDigit(char) || this.isAlpha(char);
+  }
+
+  private isDigit(char: string): boolean {
+    return char >= '0' && char <= '9';
+  }
+
+  private isAlpha(char: string): boolean {
+    return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z');
+  }
+
+  private isIdentifierChar(char: string): boolean {
+    return this.isAlphaNum(char) || char === '_' || char === '-';
   }
 
   private unexpectedToken(token: string, pos: number) {
@@ -263,75 +261,8 @@ export class Expression {
   constructor(readonly tokens: (string | Token)[]) {}
 
   eval(context: Record<string, any> = {}): string {
-    const res: unknown[] = [];
-
-    for (const token of this.tokens) {
-      if (typeof token === 'string') {
-        res.push(token);
-      } else {
-        const { value } = token.eval(context, []);
-        res.push(value);
-      }
-    }
-
-    return res.join('');
+    return this.tokens
+      .map(token => (typeof token === 'string' ? token : token.eval(context, []).value))
+      .join('');
   }
-
-  //variables(prefix: string[] = []): string[] {
-  //  const result = new Set<string>();
-  //
-  //  for (const token of this.tokens) {
-  //    if (token && typeof token !== 'string') {
-  //      this.collectVariables(token, result, prefix);
-  //    }
-  //  }
-  //
-  //  return [...result];
-  //}
-
-  // private collectVariables(node: Token, result: Set<string>, path: string[] = []): void {
-  //   const [type, ...args] = node;
-  //
-  //   switch (type) {
-  //     case OpType.Or:
-  //     case OpType.And: {
-  //       assert(Array.isArray(args[0]) && Array.isArray(args[1]), 'Or and And must have two tokens');
-  //       this.collectVariables(args[0], result, path);
-  //       this.collectVariables(args[1], result, path);
-  //       break;
-  //     }
-  //     case OpType.MemberAccess: {
-  //       assert(
-  //         Array.isArray(args[0]) && Array.isArray(args[1]),
-  //         'MemberAccess must have two tokens',
-  //       );
-  //       if (path.length > 0) {
-  //         const memberExpr = args[0];
-  //         if (memberExpr[0] === OpType.Identifier && memberExpr[1] === path[0]) {
-  //           this.collectVariables(args[1], result, path.slice(1));
-  //         }
-  //       } else {
-  //         const memberExpr = args[0];
-  //         if (memberExpr[0] === OpType.Identifier) {
-  //           result.add(memberExpr[1]);
-  //         } else {
-  //           this.collectVariables(memberExpr, result, path);
-  //         }
-  //       }
-  //       break;
-  //     }
-  //     case OpType.Identifier: {
-  //       assert(typeof args[0] === 'string', 'Identifier must be a string');
-  //       const id = args[0];
-  //       if (path.length === 0) {
-  //         result.add(id);
-  //       } else if (path.length === 1 && path[0] === id) {
-  //       } else if (path.length > 0 && path[0] !== id) {
-  //       } else {
-  //         result.add(id);
-  //       }
-  //       break;
-  //     }
-  //   }
-  // }
 }
